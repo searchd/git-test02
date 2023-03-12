3c18934ec3987aeceeb7e3ab963f15456e2f7662
searchd <MShuangjiabao@163.com>
2023-03-12 21:49:12 +0800

    4

--- src/main/java/com/j/test.java
+++ src/main/java/com/j/test.java
@@ -11,6 +11,8 @@ public class test {
 		System.out.println("hello");
 		System.out.println("hello");
 		System.out.println("hello");
-	}
+		System.out.println("hot-fix");
+		System.out.println("hot-fix");
 	
+	}
 }
