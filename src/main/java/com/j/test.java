b4ba4e4fd744b7a98b4b96c634fef67ebecc54b3
searchd <MShuangjiabao@163.com>
2023-03-12 21:41:25 +0800

    3

--- src/main/java/com/j/test.java
+++ src/main/java/com/j/test.java
@@ -9,6 +9,8 @@ package com.j;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
 	}
 	
 }
